938a7d19ad195acb48bb7b949b94019620297400
Shawn Cicoria <github@cicoria.com>
2022-02-04 12:11:01 -0500

    fix

--- try2.sh
+++ try2.sh
@@ -2,4 +2,4 @@
 
 set -a; source .env; set +a
 
-dotnet ./tmp/tools/Kusto.Cli.dll "https://scicoriadevloop.eastus.kusto.windows.net;Fed=true" -script:./s1.txt
+dotnet tools/kusto/tools/Kusto.Cli.dll "https://scicoriadevloop.eastus.kusto.windows.net;Fed=true" -script:./s1.txt
